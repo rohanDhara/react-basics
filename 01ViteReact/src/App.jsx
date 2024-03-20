5f9e2f0cacc70dc7f7928b6f60bb5c0804f5c47f
rohan30 <dhara.rohan132@gmail.com>
2024-03-20 21:51:16 +0530

    first react

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Fruit from "./container.jsx"
+function App() {
+
+  return (
+    <>
+      <Fruit />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01ViteReact/src/container.jsx
@@ -0,0 +1,7 @@
+function Fruit(){
+    return (
+        <h1>fruit is inside container</h1>
+    )
+}
+
+export default Fruit;
