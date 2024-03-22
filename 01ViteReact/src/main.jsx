30e4d317cd5b0ed1596e7ac6e0d3f8f56f1ba49e
rohan30 <dhara.rohan132@gmail.com>
2024-03-23 00:10:17 +0530

    manipulation

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return (
+        <div>
+            <h1>from custom App $$</h1>
+        </div>
+    )
+}
+const CustomElement = {
+    type:'a',
+    props:{
+        href:'https://www.google.com',
+        target:'_blank'
+    },
+    children : "add me to an element"
+}
+const newElement = (
+    <a href="https://www.google.com" target='_blank'>click to visist google once</a>
+)
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
   
-    <App />
+    newElement 
     
 )
