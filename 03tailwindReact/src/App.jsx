fc92b8ee2a0417e5bd0a1f41d638aa1529e3e15f
rohan30 <dhara.rohan132@gmail.com>
2024-09-23 18:17:23 +0530

    react with tailwind for props learning

---
+++ 03tailwindReact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-violet-300 p-5 rounded-xl text-black'>Hello I'm using Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
