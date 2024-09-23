79ac7b3e30b8aaab9550112696c002b7612ed5b0
rohan30 <dhara.rohan132@gmail.com>
2024-09-23 15:38:18 +0530

    how react render the elements

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -9,6 +9,7 @@ function MyApp(){
         </div>
     )
 }
+//we are defining the properties on our own here but using render function of react that's why it is not working
 const CustomElement = {
     type:'a',
     props:{
@@ -17,12 +18,19 @@ const CustomElement = {
     },
     children : "add me to an element"
 }
+//it will work as react render method 
 const newElement = (
     <a href="https://www.google.com" target='_blank'>click to visist google once</a>
+)
+//how react creates method
+const reactElement = React.createElement(
+    'h1',
+    {href:'https://www.google.com',target:'_blank'},
+    "Click here to visit google"
 )
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   
-    newElement 
+    reactElement
     
 )
