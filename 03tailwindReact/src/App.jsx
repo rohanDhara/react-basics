978073ff2642ccefda765bb939e0b91a401f9405
rohan30 <dhara.rohan132@gmail.com>
2024-09-23 23:30:01 +0530

    props learning

--- 03tailwindReact/src/App.jsx
+++ 03tailwindReact/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <h1 className='bg-violet-300 p-5 rounded-xl text-black'>Hello I'm using Tailwind CSS</h1>
+      <h1 className="bg-violet-300 p-5 rounded-xl text-black">
+        Hello I'm using Tailwind CSS
+      </h1>
+      <Card cardName="India" speciality="Justice for R.G Kar" />
+      <Card cardName="London" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
